17ec954d5a0613f4fb4ff9313493479f4cffc479
Cyril Strone <77802640+CyrilStrone@users.noreply.github.com>
2025-10-10 02:20:33 +0700

    feat

--- src/contexts/context-language/context.tsx
+++ src/contexts/context-language/context.tsx
@@ -1,27 +1,27 @@
-import { ILanguageKeys } from '@jenesei-software/jenesei-kit-react/types'
-import { FC, createContext, useCallback, useContext, useEffect, useMemo, useState } from 'react'
-import { I18nextProvider, useTranslation } from 'react-i18next'
+import { browserLng, fallbackLng, supportedLngs } from '@local/core/i18n';
 
-import { browserLng, fallbackLng, supportedLngs } from '@local/core/i18n'
+import { ILanguageKeys } from '@jenesei-software/jenesei-kit-react/types';
+import { createContext, FC, useCallback, useContext, useEffect, useMemo, useState } from 'react';
+import { I18nextProvider, useTranslation } from 'react-i18next';
 
-import { LanguageContextProps, ProviderLanguageProps } from '.'
+import { LanguageContextProps, ProviderLanguageProps } from '.';
 
-const LanguageContext = createContext<LanguageContextProps | null>(null)
+const LanguageContext = createContext<LanguageContextProps | null>(null);
 
 export const useLanguage = () => {
-  const context = useContext(LanguageContext)
+  const context = useContext(LanguageContext);
   if (!context) {
-    throw new Error('useLanguage must be used within an ProviderLanguage')
+    throw new Error('useLanguage must be used within an ProviderLanguage');
   }
-  return context
-}
+  return context;
+};
 
-export const ProviderLanguage: FC<ProviderLanguageProps> = props => {
-  const { i18n } = useTranslation()
-  const [isError, setIsError] = useState(false)
+export const ProviderLanguage: FC<ProviderLanguageProps> = (props) => {
+  const { i18n } = useTranslation();
+  const [isError, setIsError] = useState(false);
 
   const changeLng: LanguageContextProps['changeLng'] = useCallback(
-    lng => {
+    (lng) => {
       // changePreview({
       //   visible: true,
       //   content: (
@@ -33,28 +33,28 @@ export const ProviderLanguage: FC<ProviderLanguageProps> = props => {
       i18n
         .changeLanguage(lng)
         .then(() => {
-          setIsError(false)
+          setIsError(false);
         })
         .catch(() => {
-          setIsError(true)
+          setIsError(true);
         })
         .finally(() => {
           // changePreview({
           //   visible: false
           // })
-        })
+        });
     },
-    [i18n]
-  )
+    [i18n],
+  );
 
-  const lng = useMemo(() => i18n.language as ILanguageKeys, [i18n.language])
-  const localFallbackLng = useMemo(() => fallbackLng, [])
-  const localSupportedLngs = useMemo(() => supportedLngs, [])
-  const localBrowserLng = useMemo(() => browserLng, [])
+  const lng = useMemo(() => i18n.language as ILanguageKeys, [i18n.language]);
+  const localFallbackLng = useMemo(() => fallbackLng, []);
+  const localSupportedLngs = useMemo(() => supportedLngs, []);
+  const localBrowserLng = useMemo(() => browserLng, []);
 
   useEffect(() => {
-    document.documentElement.lang = lng
-  }, [lng])
+    document.documentElement.lang = lng;
+  }, [lng]);
 
   return (
     <I18nextProvider i18n={i18n}>
@@ -65,11 +65,11 @@ export const ProviderLanguage: FC<ProviderLanguageProps> = props => {
           browserLng: localBrowserLng,
           lng,
           isError,
-          changeLng
+          changeLng,
         }}
       >
         {props.children}
       </LanguageContext.Provider>
     </I18nextProvider>
-  )
-}
+  );
+};
