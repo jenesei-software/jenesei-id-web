1cdffd6d16de5d47b389c47d554f9f778f796d3b
Cyril Strone <77802640+CyrilStrone@users.noreply.github.com>
2025-10-11 02:48:46 +0700

    feat

--- src/contexts/context-sw/context.tsx
+++ src/contexts/context-sw/context.tsx
@@ -1,8 +1,8 @@
 import { useEnvironment } from '@local/hooks/use-environment';
 
-import { createContext, FC, useCallback, useContext, useEffect, useMemo, useState } from 'react';
+import { createContext, FC, useCallback, useContext, useMemo, useState } from 'react';
 
-import { registerSW } from 'virtual:pwa-register';
+// import { registerSW } from 'virtual:pwa-register';
 import { ProviderSWProps, SWContextProps } from '.';
 
 const SWContext = createContext<SWContextProps | null>(null);
@@ -15,52 +15,52 @@ export const useSW = () => {
 
 const STORAGE_KEY = 'app-sw-version';
 const getStoredVersion = () => localStorage.getItem(STORAGE_KEY);
-const setStoredVersion = (v: string) => localStorage.setItem(STORAGE_KEY, v);
+// const setStoredVersion = (v: string) => localStorage.setItem(STORAGE_KEY, v);
 
 export const ProviderSW: FC<ProviderSWProps> = ({ children }) => {
   const env = useEnvironment();
   const appVersion = useMemo(() => env.version, [env.version]);
 
-  const [status, setStatus] = useState<SWContextProps['status']>('idle');
-  const [isHasNewVersion, setIsHasNewVersion] = useState<SWContextProps['isHasNewVersion']>(false);
-  const [isOfflineReady, setIsOfflineReady] = useState<SWContextProps['isOfflineReady']>(false);
-  const [versionCurrent, setVersionCurrent] = useState<SWContextProps['versionCurrent']>(
+  const [status] = useState<SWContextProps['status']>('idle');
+  const [isHasNewVersion] = useState<SWContextProps['isHasNewVersion']>(false);
+  const [isOfflineReady] = useState<SWContextProps['isOfflineReady']>(false);
+  const [versionCurrent] = useState<SWContextProps['versionCurrent']>(
     getStoredVersion() ?? appVersion,
   );
-  const [versionLatest, setVersionLatest] = useState<SWContextProps['versionLatest']>(null);
+  const [versionLatest] = useState<SWContextProps['versionLatest']>(null);
 
-  const [updateSW, setUpdateSW] = useState<((reload?: boolean) => void) | null>(null);
+  const [updateSW] = useState<((reload?: boolean) => void) | null>(null);
 
-  useEffect(() => {
-    setStatus('loading');
+  // useEffect(() => {
+  //   setStatus('loading');
 
-    const sw = registerSW({
-      immediate: true,
-      onNeedRefresh() {
-        setIsHasNewVersion(true);
-      },
-      onOfflineReady() {
-        setIsOfflineReady(true);
-      },
-      onRegisteredSW(swUrl, registration) {
-        setStatus('ready');
-        if (registration?.active) {
-          const swVersion =
-            new URL(swUrl, location.origin).searchParams.get('__WB_REVISION__') ?? Date.now().toString();
-          setStoredVersion(swVersion);
-          setVersionLatest(swVersion);
-          if (swVersion !== appVersion) setIsHasNewVersion(true);
-          setVersionCurrent(appVersion);
-        }
-      },
-      onRegisterError(err) {
-        console.error('SW registration error', err);
-        setStatus('error');
-      },
-    });
+  //   const sw = registerSW({
+  //     immediate: true,
+  //     onNeedRefresh() {
+  //       setIsHasNewVersion(true);
+  //     },
+  //     onOfflineReady() {
+  //       setIsOfflineReady(true);
+  //     },
+  //     onRegisteredSW(swUrl, registration) {
+  //       setStatus('ready');
+  //       if (registration?.active) {
+  //         const swVersion =
+  //           new URL(swUrl, location.origin).searchParams.get('__WB_REVISION__') ?? Date.now().toString();
+  //         setStoredVersion(swVersion);
+  //         setVersionLatest(swVersion);
+  //         if (swVersion !== appVersion) setIsHasNewVersion(true);
+  //         setVersionCurrent(appVersion);
+  //       }
+  //     },
+  //     onRegisterError(err) {
+  //       console.error('SW registration error', err);
+  //       setStatus('error');
+  //     },
+  //   });
 
-    setUpdateSW(() => sw);
-  }, [appVersion]);
+  //   setUpdateSW(() => sw);
+  // }, [appVersion]);
 
   const onUpdate = useCallback(() => {
     if (updateSW) updateSW(true);
