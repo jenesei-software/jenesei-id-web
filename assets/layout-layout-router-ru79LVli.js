0acc7d6545527f1331bab68342f72422be5f7865
CyrilStrone <CyrilStrone@users.noreply.github.com>
2025-10-18 21:02:46 +0000

    deploy: c231376c722b19fc00a35116a9c46d6ee4b5a1ed

---
+++ assets/layout-layout-router-ru79LVli.js
@@ -0,0 +1,1 @@
+import{j as r,ab as t}from"./vendor-hvd7BaYo.js";import{q as o,r as e}from"./layout-layout-root-CgbApf7m.js";function a(){return r.jsx(t,{router:e,context:{queryClient:o}})}export{a as L};
